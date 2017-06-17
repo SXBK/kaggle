c351050bcce3423af33fe3d7e3a63d953e869775
uartist <uartist@localhost.localdomain>
2017-06-17 16:42:10 +0800

    Add multi feature

--- zillow/multi_feautre.py
+++ zillow/multi_feautre.py
@@ -1,16 +1,3 @@
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load in 
-
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-
-# Input data files are available in the "data/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
-
-from subprocess import check_output
-print(check_output(["ls", "data"]).decode("utf8"))
-
 # Any results you write to the current directory are saved as output.
 import numpy as np
 import pandas as pd
@@ -19,10 +6,6 @@ import gc
 import sklearn
 import commands
 import matplotlib.pyplot as plt
-
-def log(sta, info):
-	#commands.getstatusoutput('cat "' + sta+ ': ' + info + '">> out/mse.log')
-	return 1
 
 print('Loading data ...')
 
@@ -36,22 +19,35 @@ for c, dtype in zip(prop.columns, prop.dtypes):
 df_train = train.merge(prop, how='left', on='parcelid')
 
 #drop miss to much
-df_missing = df_train.isnull().sum(axis=1).reset_index()
-df_missing.columns=['index','missingnum']
-df_train = df_train.merge(df_missing, left_index=True, right_on='index')
-df_train=df_train.ix[df_train['missingnum']<56]
+#df_missing = df_train.isnull().sum(axis=1).reset_index()
+#df_missing.columns=['index','missingnum']
+#df_train = df_train.merge(df_missing, left_index=True, right_on='index')
+#df_train=df_train.ix[df_train['missingnum']<56]
 
 x_train = df_train.drop(['parcelid', 'logerror', 'transactiondate', 'propertyzoningdesc', 'propertycountylandusecode'], axis=1)
 y_train = df_train['logerror'].values
 print(x_train.shape, y_train.shape)
 
-
-train_columns = x_train.columns
+x_train = x_train.drop(['rawcensustractandblock', 'regionidcity', 'regionidneighborhood','regionidzip', 'calculatedfinishedsquarefeet', 'finishedsquarefeet12',
+  'yearbuilt', 'structuretaxvaluedollarcnt' ,'taxvaluedollarcnt',
+   'landtaxvaluedollarcnt', 'taxamount', 'lotsizesquarefeet',
+    'censustractandblock', 'latitude'], axis=1)
 
 for c in x_train.dtypes[x_train.dtypes == object].index.values:
     x_train[c] = (x_train[c] == True)
 
+#x_train['taxdelinquencyflag'] = (x_train['taxdelinquencyflag'] == 'Y')
+
 del df_train; gc.collect()
+
+train_columns = x_train.columns
+for i in range(train_columns.size):
+	for j in range(train_columns.size):
+		if i < j:
+			x_train[str(i)+'*'+str(j)]=x_train[train_columns[i]]*x_train[train_columns[j]]
+	
+train_columns = x_train.columns
+all_columns = x_train
 
 split = 90000
 x_train, y_train, x_valid, y_valid = x_train[:split], y_train[:split], x_train[split:], y_train[split:]
@@ -63,55 +59,59 @@ d_valid = lgb.Dataset(x_valid, label=y_valid)
 
 params = {}
 params['max_bin'] = 10
-params['learning_rate'] = 0.0021 # shrinkage_rate
+params['learning_rate'] = 0.003 # shrinkage_rate
 params['boosting_type'] = 'gbdt'
 params['objective'] = 'regression'
 params['metric'] = 'l1'          # or 'mae'
-params['sub_feature'] = 0.5      # feature_fraction 
+params['sub_feature'] = 0.65      # feature_fraction 
 params['bagging_fraction'] = 0.85 # sub_row
-params['bagging_freq'] = 40
+params['bagging_freq'] = 20
 params['num_leaves'] = 512        # num_leaf
 params['min_data'] = 500         # min_data_in_leaf
 params['min_hessian'] = 0.05     # min_sum_hessian_in_leaf
 
 watchlist = [d_valid]
 labellist = []
-for i in [400, 600, 800, 1000]:
+for i in [150]:
 	train_time = i
 	clf = lgb.train(params, d_train, train_time, watchlist)
 	fig, ax = plt.subplots(figsize=(12,16))
 	lgb.plot_importance(clf, ax=ax, title=str(train_time))
 	labels = [int(j.get_text()[7:]) for j in ax.get_yticklabels()]
 	labellist.append(labels)
-#plt.show()
+	#plt.show()
 
 
-p_valid = clf.predict(x_valid)
+#p_valid = clf.predict(x_valid)
 
-for i in range(10):
-	print('valid:', y_valid[i],'predict:', p_valid[i])
-	log('origin lgbm','valid:'+ str(y_valid[i])+' predict:'+str(p_valid[i]))
-print(sklearn.metrics.mean_squared_error(y_valid, p_valid))
-log('origin lgbm',str(sklearn.metrics.mean_squared_error(y_valid, p_valid)))
+#for i in range(10):
+	#print('valid:', y_valid[i],'predict:', p_valid[i])
+#print(sklearn.metrics.mean_squared_error(y_valid, p_valid))
 
 bset=set()
 sset=set()
-for k, labels in zip([400, 600, 800, 1000], labellist):
+for k, labels in zip([150], labellist):
+	lenght = -int(len(labels)*0.2)
 	print('Iter is:', k)
 	print('Best 15:', labels[:15])
 	print('Sbst 15:', labels[-15:])
-	if len(bset)==0 and len(sset)==0:    #first time
-		bset=set(labels[:15])
-		sset=set(labels[-15:])
-	else:
-		bset=bset&set(labels[:15])
-		sset=sset&set(labels[-15:])
+	bset=set(labels[:15])
+	sset=set(labels[lenght:])
 
-print(list(bset))
 print(train_columns.values[list(bset)])
-print(list(sset))
-print(train_columns.values[list(sset)])
+#print(train_columns.values[list(sset)])
 
+x_train = all_columns.drop(train_columns.values[list(sset)], axis=1)
+
+x_train,  x_valid = x_train[:split], x_train[split:]
+x_train = x_train.values.astype(np.float32, copy=False)
+x_valid = x_valid.values.astype(np.float32, copy=False)
+
+d_train = lgb.Dataset(x_train, label=y_train)
+d_valid = lgb.Dataset(x_valid, label=y_valid)
+
+watchlist = [d_valid]
+clf = lgb.train(params, d_train, 150, watchlist)
 
 del d_train, d_valid; gc.collect()
 del x_train, x_valid; gc.collect()
