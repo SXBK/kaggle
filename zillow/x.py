990f6dc1cf41f3bb2e1ba8b2bb0df59d6ebffe6a
Michael <lordli@qq.com>
2017-06-22 10:24:51 +0800

    Modify add index

--- zillow/x.py
+++ zillow/x.py
@@ -10,36 +10,32 @@ from keras.layers import LSTM
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error
 
-total = ts.get_k_data('600848', start='2011-01-05')
-total = total.drop(['date','code'], axis=1)
+total = ts.get_k_data('600848', start='2011-01-05').drop(['code'], axis=1)
+index = ts.get_k_data('000001', start='2011-01-05').drop(['code'], axis=1)
+total = total.merge(index, how='left', on='date')
+total = total.drop(['date'], axis=1)
 print(total.head())
 
 #scaler = StandardScaler()
 #total = scaler.fit_transform(total)
-total = (total - total.mean())/total.var()
-print(total.head())
+#total = (total - total.mean())/(total.var()+1e-8)
 
-y = total['open']
-#x = total.drop(['open'], axis=1)
-x = total
+y = total['open_x'].values[1:]
+x = np.reshape(total.values,(-1,1,10))[:-1]
+print(x)
 
 split = 1000
-x_train, x_test, y_train, y_test = x[:split], x[split:], y[:split], y[split:]
-
-xt = np.reshape(x_train.values, (-1, 1, 5))
-yt = y_train.values
-
+x_train, x_test, y_train, y_test = x[:split,:,:], x[split:,:,:], y[:split], y[split:]
+print(x_train)
+print(y_train)
 
 #create and fit the LSTM network
 model = Sequential()
-model.add(LSTM(4, input_shape=(1, 5)))
+#model.add(LSTM(4, input_shape=(1, 10),return_sequences=True))
+model.add(LSTM(4, input_shape=(1, 10)))
 model.add(Dense(1))
-model.summary()
-
-xt = np.reshape(x_train, (-1, 1, 5))
 model.compile(loss='mse', optimizer='adam')
-model.fit(xt, yt, nb_epoch=100, batch_size=32)
+model.fit(x_train, y_train, epochs=100, batch_size=16)
 
-plt.figure()
-#plt.plot(range(y.shape[0]), y.values)
-#plt.show()
+pred = model.predict(x_test)
+print('MSE:',mean_squared_error(pred, y_test) )
