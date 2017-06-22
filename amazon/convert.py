800b457cf2eab275cf8f93a88f3ec3ffeb75b1d6
ygao12 <ygao12@users.noreply.github.com>
2017-06-22 20:24:19 +0800

    convert IMG to HDF5

---
+++ amazon/convert.py
@@ -0,0 +1,26 @@
+'''convert img to hdf5
+'''
+import glob
+import pandas as pd
+from keras.preprocessing import image
+import numpy as np
+from progressive.bar import Bar
+
+imglist = glob.glob('./train-jpg/*.jpg')
+allimg = sorted(imglist, key=lambda x: int(x[18:].split('.')[0]))
+
+imgsize = 224
+totalimg = len(imglist)
+alldata = np.zeros([totalimg * imgsize, imgsize, 3], dtype=np.uint8)
+
+pb = Bar(max_value=totalimg, fallback=True)
+pb.cursor.clear_lines(2)
+pb.cursor.save()
+for index, img in enumerate(allimg):
+    alldata[index:index+imgsize, :, :] = image.load_img(img,
+                                                        target_size=(224, 224))
+    pb.cursor.restore()
+    pb.draw(index)
+
+pddata = pd.Panel(alldata)
+pddata.to_hdf('train_amazon.hdf5', 'train_amazon')
