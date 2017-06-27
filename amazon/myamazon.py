d0d1f0b492303115870692ac7bd74092d71ab7ff
gyahbx <ygao12@qq.com>
2017-06-27 18:59:09 +0800

    add generator support

--- amazon/myamazon.py
+++ amazon/myamazon.py
@@ -1,75 +1,91 @@
-#common utils
+# common utils
 import numpy as np
 import pandas as pd
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 
+
 def f2s(pred, valid):
-	pred = np.asarray(pred, dtype=np.int)
-	valid = np.asarray(valid)
-	rp = np.bitwise_and(pred, valid).sum()
-	p = rp*1.0/pred.sum()
-	r = rp*1.0/valid.sum()
-	return 5*p*r/(4*p + r)
+    pred = np.asarray(pred, dtype=np.int)
+    valid = np.asarray(valid)
+    rp = np.bitwise_and(pred, valid).sum()
+    p = rp * 1.0 / pred.sum()
+    r = rp * 1.0 / valid.sum()
+    return 5 * p * r / (4 * p + r)
+
 
 def load_data(num, imgtimes=0):
-	ytrain = pd.read_csv('data/train_v2.csv')[:num]
-	xtrain = []
-	new_style = {'grid': False}
-	for f, l in ytrain.values:
-		img_path = 'data/train-jpg/' + f + '.jpg'
-		img = load_img(img_path, target_size=(224, 224))
-		x = img_to_array(img)
-		xtrain.append(x)
-	xtrain = np.array(xtrain)
-	ytrain = ytrain['tags'].values
-	ytrain, fl = encodeY(ytrain, num)
-	xtrain = (xtrain - xtrain.mean())/(xtrain.std()+1e-8)
-	if imgtimes > 1:
-		xtrain, ytrain = imageStrong(xtrain, ytrain, imgtimes)
-	print(ytrain.shape, xtrain.shape)
+    ytrain = pd.read_csv('data/train_v2.csv')[:num]
+    xtrain = []
+    new_style = {'grid': False}
+    for f, l in ytrain.values:
+        img_path = 'data/train-jpg/' + f + '.jpg'
+        img = load_img(img_path, target_size=(224, 224))
+        x = img_to_array(img)
+        xtrain.append(x)
+    xtrain = np.array(xtrain)
+    ytrain = ytrain['tags'].values
+    ytrain, fl = encodeY(ytrain, num)
+    xtrain = (xtrain - xtrain.mean()) / (xtrain.std() + 1e-8)
+    if imgtimes > 1:
+        xtrain, ytrain = imageStrong(xtrain, ytrain, imgtimes)
+    print(ytrain.shape, xtrain.shape)
 
-	return xtrain, ytrain, fl
+    return xtrain, ytrain, fl
+
 
 def imageStrong(x, y, imgtimes):
-	datagen = ImageDataGenerator(
-	        rotation_range=90,
-			width_shift_range=0.1,
-			height_shift_range=0.1,
-			shear_range=0.1,
-			zoom_range=0.1,
-			horizontal_flip=True,
-			fill_mode='nearest')
-	nimg = np.array([])
-	nlab = pd.DataFrame()
-	for i in range(imgtimes-1):
-		for batch in datagen.flow(x, batch_size=x.shape[0]):
-			if nimg.shape[0] < batch.shape[0]:
-				nimg = np.array(batch, copy=True)
-				#nlab = np.array(y, copy=True)
-				nlab = y.copy()
-			else:
-				nimg = np.concatenate((nimg, np.array(batch)))
-				#nlab = np.concatenate((nlab, np.array(y)))
-				nlab = nlab.append(y)
-			break
-	x = np.concatenate((x, nimg))
-	#y = np.concatenate((y, nlab))
-	y = y.append(nlab)
-	return x, y
+    datagen = ImageDataGenerator(
+        rotation_range=90,
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        shear_range=0.1,
+        zoom_range=0.1,
+        horizontal_flip=True,
+        fill_mode='nearest')
+    nimg = np.array([])
+    nlab = pd.DataFrame()
+    for i in range(imgtimes - 1):
+        for batch in datagen.flow(x, batch_size=x.shape[0]):
+            if nimg.shape[0] < batch.shape[0]:
+                nimg = np.array(batch, copy=True)
+                #nlab = np.array(y, copy=True)
+                nlab = y.copy()
+            else:
+                nimg = np.concatenate((nimg, np.array(batch)))
+                #nlab = np.concatenate((nlab, np.array(y)))
+                nlab = nlab.append(y)
+            break
+    x = np.concatenate((x, nimg))
+    #y = np.concatenate((y, nlab))
+    y = y.append(nlab)
+    return x, y
 
 
 def encodeY(tags, num):
-	allTags = set()
-	for t in tags:
-		for tag in t.split(' '):
-			allTags.add(tag)
-	allTags = list(allTags)
-	print(allTags)
-	fl = len(allTags)
-	fs = np.zeros((num, fl), dtype="uint8")
-	for j, c in zip(range(len(tags)), tags):
-		for i in range(fl):
-			fs[j, i] = 1 if c.find(allTags[i]) >= 0 else 0
-	ytrain = pd.DataFrame(data=fs, columns=allTags)
-	return ytrain, fl
+    allTags = set()
+    for t in tags:
+        for tag in t.split(' '):
+            allTags.add(tag)
+    allTags = list(allTags)
+    print(allTags)
+    fl = len(allTags)
+    fs = np.zeros((num, fl), dtype="uint8")
+    for j, c in zip(range(len(tags)), tags):
+        for i in range(fl):
+            fs[j, i] = 1 if c.find(allTags[i]) >= 0 else 0
+    ytrain = pd.DataFrame(data=fs, columns=allTags)
+    return ytrain, fl
 
+
+def load_hdfdata(num):
+    ytrain = pd.read_csv('data/train_v2.csv')[:num]
+    xtrain = np.reshape(pd.read_hdf('data/train_amazon.hdf5',
+                                    'train_amazon').values,
+                        (-1, 224, 224, 3))
+    xtrain = xtrain[:num]
+    ytrain = ytrain['tags'].values
+    ytrain, fl = encodeY(ytrain, num)
+
+    print(ytrain.shape, xtrain.shape)
+
+    return xtrain, ytrain.values, fl
--- amazon/vgg_tsf.py
+++ amazon/vgg_tsf.py
@@ -1,3 +1,6 @@
+
+'''Transform Learning using vgg16 models
+'''
 from __future__ import absolute_import
 from __future__ import print_function
 from keras.preprocessing.image import ImageDataGenerator
@@ -11,18 +14,27 @@ from keras.applications.vgg16 import VGG16
 import keras
 import os
 import numpy as np
+from numpy import random
 import pandas as pd
 import sys
-from myamazon import f2s, load_data
+from myamazon import f2s, load_data, load_hdfdata
 
-num = 100
-vnum = 100
-times = 1
+num = 40000
+
+meanArray = np.array([79.4163002113, 86.8382492145, 76.2017108336])
 
 # num is the raw data num, and you'll get num * times sample
-x, y, fl = load_data(num, times)
-x_valid, y_valid = x[vnum:], y[vnum:]
-x, y = x[:vnum], y[:vnum]
+x, y, fl = load_hdfdata(num)
+print("load {} samples finished".format(num))
+
+
+def generate_from_data(xtrain, ytrain, batch_size):
+    while True:
+        index = random.choice(num, batch_size)
+        x = np.float32(xtrain[index]) - meanArray
+        y = ytrain[index]
+        yield x, y
+
 
 model_ORI = VGG16(weights='imagenet', include_top=True)
 model = Sequential()
@@ -33,22 +45,24 @@ for i, layer in enumerate(model_ORI.layers):
     layer.trainable = False
     model.add(layer)
 model.add(Dense(2048, activation='relu'))
-#model.add(Dropout(0.5))
+# model.add(Dropout(0.5))
 model.add(Dense(2048, activation='relu'))
-#model.add(Dropout(0.5))
+# model.add(Dropout(0.5))
 model.add(Dense(fl, activation='sigmoid'))
 model.summary()
 adam = keras.optimizers.Adam(lr=1e-4, beta_1=0.9,
                              beta_2=0.999, epsilon=1e-08, decay=0.0)
 model.compile(loss='categorical_crossentropy', optimizer=adam)
-model.fit(x, y.values, epochs=int(sys.argv[1]))
+# model.fit(x, y.values, epochs=int(sys.argv[1]))
+model.fit_generator(generate_from_data(x, y, 50), steps_per_epoch=5000,
+                    epochs=1, max_q_size=1, workers=1)
 
 model.save('model_vgg.h5')
 
-y_valid = y[:5].values
+y_valid = y[:5]
 x_valid = x[:5]
 pred = model.predict(x_valid)
-print(y_valid[:,:])
+print(y_valid[:, :])
 pred[pred > 0.5] = 1
 pred[pred <= 0.5] = 0
 print(pred[:, :])
